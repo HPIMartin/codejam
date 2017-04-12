e46e55e0c4dbd7d686d0ef5f7daab6573b153b38
Hendrik Graupner <hendrik.graupner@hpi.de>
2017-04-12 23:36:44 +0200

    added all problem sets as acceptance tests

--- src/main/java/codejam/MainClass.java
+++ src/main/java/codejam/MainClass.java
@@ -1,10 +1,10 @@
 package codejam;
 
-import codejam.year2017.qualification.ProblemBTidyNumbers;
+import codejam.year2017.qualification.ProblemAOversizedPancakeFlipper;
 
 public class MainClass {
 	public static void main(String[] args) {
-		Problem<?, ?> problem = new ProblemBTidyNumbers();
+		Problem<?, ?> problem = new ProblemAOversizedPancakeFlipper();
 		problem.solve(args);
 	}
 }
